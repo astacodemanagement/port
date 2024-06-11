a54d59b7a4fe4cb46705f83c2538a0242ca91206
Irfan <qcutterz@gmail.com>
2024-06-11 15:14:08 +0700

    Validate register

--- resources/views/front/compro-1/layouts/navbar.blade.php
+++ resources/views/front/compro-1/layouts/navbar.blade.php
@@ -2,9 +2,11 @@
 <nav class="navbar fixed-top">
     <div class="container-fluid">
         <div class="d-flex">
+            <a href="{{ route('home') }}">
             <img src="{{ asset('frontend') }}/assets/logo/logo.png" alt="">
-            <a class="navbar-brand text-white fw-semibold mt-2 col-sm-hidden">PT. PUTRA SAHABAT
-                INTERNATIONAL</a>
+            <span class="navbar-brand text-white fw-semibold mt-2 col-sm-hidden">PT. PUTRA SAHABAT
+                INTERNATIONAL</span>
+            </a>
         </div>
         <div class="d-flex">
             <a href="#" class="link-pages">Worker</a>
