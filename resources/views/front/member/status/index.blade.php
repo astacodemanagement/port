8872cd820f0b2b68622016edf00c46229b16aaf4
PramAhmad <pramuditaadit2810@gmail.com>
2024-06-26 21:11:18 +0700

    remove depedency yang gak dipakai di status proses

--- resources/views/front/member/status/index.blade.php
+++ resources/views/front/member/status/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('title', 'Status Proses Lamaran Kerja')
 @push('css')
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.1/dist/cdn.min.js"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
 @endpush
 @section('content')
@@ -136,9 +135,3 @@
     <!-- Button trigger modal -->
 
 @endsection
-@push('js')
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-@endpush
