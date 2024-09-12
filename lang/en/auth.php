916f9b21e5e5d6786371b629139be50bbecff396
PramAhmad <pramuditaadit2810@gmail.com>
2024-09-12 14:54:49 +0700

    change msg error login

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Email Dan Password Yang Di Masukan Salah',
+    'password' => 'Password Yang Di Masukan Salah',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
