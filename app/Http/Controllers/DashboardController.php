cb1c9562414f5da0a7904ba2f24f0b84bf24387b
PramAhmad <pramuditaadit2810@gmail.com>
2024-11-21 17:52:45 +0700

    dashboard update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use App\Models\Kandidat;
 use App\Models\Negara;
+use App\Models\Pendaftaran;
 use App\Models\Pengaduan;
 use App\Models\Seleksi;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -18,15 +20,37 @@ class DashboardController extends Controller
       $data['count_negara'] = Negara::count();
       $data['pelamar'] = Kandidat::limit(5)->get();
       $data['pengaduan'] = Pengaduan::limit(7)->get();
-       $data['belum_verifikasi'] = Kandidat::where('status','Pending')->count();
+      $data['belum_verifikasi'] = Kandidat::where('status','Pending')->count();
       $data['verifikasi'] = Kandidat::where('status','Verifikasi')->count();
       $data['reject'] = Kandidat::where('status','Reject')->count();
       $data['backlist'] = Job::where('status','Backlist')->count();  
-      $data['jobactive'] = Job::where('tanggal_tutup','>=',date('Y-m-d'))->get();
-      $data['jobinactive'] = Job::where('tanggal_tutup','<',date('Y-m-d'))->get();
+      $data['jobactive'] = Job::where('tanggal_tutup','>=',date('Y-m-d'))->limit(5)->get();
+      $data['jobinactive'] = Job::where('tanggal_tutup','<',date('Y-m-d'))->limit(5)->get();
       $data['interview'] = Seleksi::where('status','Interview')->get();
-      // return $data['jobactive'];
-    return view('back.dashboard',$data);
+      $data['provinsi_count'] =  Kandidat::select('provinsi_id', DB::raw('COUNT(*) as total'))
+      ->groupBy('provinsi_id')
+      ->with('provinsi')
+      ->orderBy('total','desc')
+      ->get();
+      $data['kategori_job_count'] = Pendaftaran::select('kategori_job_id', DB::raw('COUNT(*) as total'))
+      ->groupBy('kategori_job_id')
+      ->with('kategoriJob')
+      ->orderBy('total','desc')
+      ->get();
+      $data['country_count'] = Job::select('negara_id', DB::raw('COUNT(*) as total'))
+      ->groupBy('negara_id')
+      ->with('negara')
+      ->orderBy('total','desc')
+      ->limit(6)
+      ->get();
+      // pendidikan count
+      $data['pendidikan_count'] = Kandidat::select('pendidikan', DB::raw('COUNT(*) as total'))
+      ->groupBy('pendidikan')
+      ->orderBy('total','desc')
+      ->get();
+   // return $data['pendidikan_count'];
+      // return $data['country_count'];
+      return view('back.dashboard',$data);
    }
   
 }
